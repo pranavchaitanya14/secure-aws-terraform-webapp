858c61f33e2c439284b71fce34b3eb10ee47c3de
P. Pranav chaitanya <pranavchaitanya14@gmail.com>
2026-02-10 13:52:32 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+function submitData() {
+
+    let name = document.getElementById("name").value;
+    let roll = document.getElementById("roll").value;
+
+    if (name === "" || roll === "") {
+        alert("Please fill all fields!");
+        return;
+    }
+
+    document.getElementById("result").innerHTML =
+        "✅ Welcome " + name + " (Roll: " + roll + ")";
+
+}
